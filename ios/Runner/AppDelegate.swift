0f6754dd8312a52578d348e6a1203890f249cb65
Chris Whong <chris.m.whong@gmail.com>
2024-11-18 01:05:30 -0500

    restore AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,11 +7,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if let associatedDomains = Bundle.main.object(forInfoDictionaryKey: "com.apple.developer.associated-domains") as? [String] {
-        print("Associated Domains: \(associatedDomains)")
-    } else {
-        print("No Associated Domains found")
-    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
